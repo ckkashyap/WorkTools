1d95a08f3e8f335aa81b9bf280ebeaba2c0f57bf
CK Kashyap <ckk@microsoft.com>
2020-10-07 11:13:57 -0700

    Empty solution

---
+++ ServiceBusListener/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceBusListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
